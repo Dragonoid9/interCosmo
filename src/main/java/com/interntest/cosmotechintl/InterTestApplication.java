64a8131c5268bcfb1ce3f14d3874dedb05d2c5e0
Dragonoid9 <sadip.khadka.96@gmail.com>
2024-12-02 16:46:39 +0545

    First commit with only Project init. Starting point with MySql connection only nothing more.

---
+++ src/main/java/com/interntest/cosmotechintl/InterTestApplication.java
@@ -0,0 +1,13 @@
+package com.interntest.cosmotechintl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interntest/cosmotechintl/InterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interntest.cosmotechintl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
