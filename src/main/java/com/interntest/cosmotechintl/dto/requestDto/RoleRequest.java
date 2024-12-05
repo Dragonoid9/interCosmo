05ebf73836673217e0f8aae574e10a7c00ba446a
Dragonoid9 <sadip.khadka.96@gmail.com>
2024-12-05 15:42:53 +0545

    RoleRequest has been updated.

---
+++ src/main/java/com/interntest/cosmotechintl/dto/requestDto/RoleRequest.java
@@ -0,0 +1,13 @@
+package com.interntest.cosmotechintl.dto.requestDto;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleRequest {
+    private String name;
+}
